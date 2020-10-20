fac8678b6e205e4a387a73f4ef1ab6e1457be377
chand1012 <chandlerl2000@gmail.com>
2020-10-20 10:40:10 -0400

    added requirements, remove rfeed

--- feed.py
+++ feed.py
@@ -1,18 +1,8 @@
 from datetime import datetime
-
-from rfeed import *
+from xml.etree.ElementTree import ElementTree
 
 
 def generate_feed(data, server):
-    items = []
     servers = data.get('servers')
-    server = servers[server]
-
-    statString = f''
-
-    items += [Item(
-        title='Stats',
-        link=f'https://{server}',
-        pubDate=datetime.now(),
-        description=f''
-    )]
+    server = servers.get(server)
+    
